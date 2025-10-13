140930fd2c7b3e35ad3c342d5cc4b64051094203
KingEddeh <28296038+KingEddeh@users.noreply.github.com>
2025-10-13 22:11:28 +0800

    feat: enhance admin interfaces for user, agency, emergency, public info, and responder models with custom admin classes

--- backend/emergencies/admin.py
+++ backend/emergencies/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from .models import EmergencyType, EmergencyReport, EmergencyVerification, UserEvaluation
 
-admin.site.register(EmergencyType)
-admin.site.register(EmergencyReport)
-admin.site.register(EmergencyVerification)
-admin.site.register(UserEvaluation)
+class EmergencyTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'icon_type')
+    search_fields = ('name',)
+
+class EmergencyReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'emergency_type', 'user', 'verification_status', 'status', 'date_created')
+    list_filter = ('emergency_type', 'verification_status', 'status')
+    search_fields = ('id', 'user__email', 'details')
+    date_hierarchy = 'date_created'
+
+class EmergencyVerificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'report', 'user', 'vote', 'date_created')
+    list_filter = ('vote',)
+    search_fields = ('report__id', 'user__email', 'details')
+    date_hierarchy = 'date_created'
+
+class UserEvaluationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'report', 'user', 'stars', 'did_app_guide_clearly', 'completion_speed')
+    list_filter = ('stars', 'did_app_guide_clearly', 'completion_speed', 'confidence_level')
+    search_fields = ('report__id', 'user__email', 'improvement_suggestion')
+    date_hierarchy = 'date_created'
+
+admin.site.register(EmergencyType, EmergencyTypeAdmin)
+admin.site.register(EmergencyReport, EmergencyReportAdmin)
+admin.site.register(EmergencyVerification, EmergencyVerificationAdmin)
+admin.site.register(UserEvaluation, UserEvaluationAdmin)
--- backend/responders/admin.py
+++ backend/responders/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Responder
 
-admin.site.register(Responder)
+class ResponderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'agency')
+    list_filter = ('agency',)
+    search_fields = ('user__email', 'agency__name')
+
+admin.site.register(Responder, ResponderAdmin)
