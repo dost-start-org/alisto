2d634b01c449230ded522b1ac42267477d3ccad7
omoraisu <dapeladas@up.edu.ph>
2025-10-23 14:16:23 +0800

    Use seeder instead of fixed value for data

--- backend/agencies/management/commands/seed_agencies.py
+++ backend/agencies/management/commands/seed_agencies.py
@@ -15,33 +15,33 @@ class Command(BaseCommand):
             {
                 "name": "Bureau of Fire Protection",
                 "logo_url": "https://bfp.gov.ph/wp-content/uploads/2023/04/BFP-OFFICIAL-LOGO.png",
-                "hotline_number": "123-456-7890",
-                "latitude": 40.7128,
-                "longitude": -74.0060,
+                "hotline_number": seeder.faker.msisdn()[:11],
+                "latitude": seeder.faker.latitude(),
+                "longitude": seeder.faker.longitude(),
                 "emergency_types": ["Fire", "Flood"]
             },
             {
                 "name": "Philippine National Police",
                 "logo_url": "https://pnp.gov.ph/wp-content/uploads/2022/03/cropped-logo_512.png",
-                "hotline_number": "098-765-4321",
-                "latitude": 34.0522,
-                "longitude": -118.2437,
+                "hotline_number": seeder.faker.msisdn()[:11],
+                "latitude": seeder.faker.latitude(),
+                "longitude": seeder.faker.longitude(),
                 "emergency_types": ["Active Crime"]
             },
             {
                 "name": "Ambulance Service",
                 "logo_url": "https://example.com/logos/ems.png",
-                "hotline_number": "555-555-5555",
-                "latitude": 41.8781,
-                "longitude": -87.6298,
+                "hotline_number": seeder.faker.msisdn()[:11],
+                "latitude": seeder.faker.latitude(),
+                "longitude": seeder.faker.longitude(),
                 "emergency_types": ["Medical Emergency"]
             },
             {
                 "name": "Disaster Response Agency",
                 "logo_url": "https://example.com/logos/dra.png",
-                "hotline_number": "444-444-4444",
-                "latitude": 29.7604,
-                "longitude": -95.3698,
+                "hotline_number": seeder.faker.msisdn()[:11],
+                "latitude": seeder.faker.latitude(),
+                "longitude": seeder.faker.longitude(),
                 "emergency_types": ["Flood", "Fire", "Medical Emergency"]
             },
         ]
