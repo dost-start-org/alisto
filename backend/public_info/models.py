c65f9c4835de0d1dff3c3319d9720f074eb84c26
omoraisu <dapeladas@up.edu.ph>
2025-10-29 22:03:40 +0800

    Add user emergency contact model

--- backend/public_info/models.py
+++ backend/public_info/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 import uuid
 
 class EmergencyContact(models.Model):
@@ -15,6 +16,17 @@ class EmergencyContact(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.contact_number}"
+
+class UserEmergencyContact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=50)
+    relationship = models.CharField(max_length=100, null=True, blank=True)
+    photo_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.user}'s {self.relationship}"
 
 class ContactRedirection(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
